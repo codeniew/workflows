590ac2423f92c0e08902b847048a13f9511cb97f
nw <niewei237@gmail.com>
2023-03-07 12:31:07 +0800

    第一次

---
+++ auto_card.py
@@ -0,0 +1,33 @@
+
+import requests
+import os
+from dingding import get_data, gaojing
+
+
+def auto_card():
+    form_data = {"token": "glados.network"}
+    url = "https://glados.one/api/user/checkin"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
+        "Cookie": os.getenv('COOKIE')
+    }
+    print("ss")
+    response = requests.post(url, data=form_data, headers=headers)
+    response_data = response.json()
+    print(response_data)
+    if response_data['message'] == "Please Try Tomorrow":
+        print("nw已重复打卡")
+        data = get_data("nw已重复打卡")
+        gaojing(data)
+    elif response_data['code'] == -2:
+        data = get_data(response_data['message'])
+        gaojing(data)
+        print(response_data['message'])
+    else:
+        print("nw打卡成功")
+        data = get_data("nw打卡成功")
+        gaojing(data)
+
+
+if __name__ == '__main__':
+    auto_card()
---
+++ dingding.py
@@ -0,0 +1,49 @@
+# filename: dingtalk.py
+import base64
+import hashlib
+import hmac
+import time
+import urllib
+import os
+import requests
+import json
+import sys
+
+def gaojing(data):
+    # 将消息提交给钉钉机器人
+    headers = {'Content-Type': 'application/json;charset=utf-8'}
+    # 注意替换钉钉群的机器人webhook
+    access_token = os.getenv('TOKEN')
+    timestamp, sign = timeSign()
+    webhook = 'https://oapi.dingtalk.com/robot/send?access_token=%s&timestamp=%s&sign=%s' % (access_token, timestamp, sign)
+    print(webhook)
+    repson = requests.post(url=webhook, data=json.dumps(data), headers=headers)
+    print(repson.text)
+
+def get_data(text_content):
+    # 返回钉钉机器人所需的文本格式
+    text = {
+        "msgtype": "text",
+        "text": {
+            "content": text_content
+        },
+    }
+    return text
+
+def timeSign():
+    timestamp = str(round(time.time() * 1000))
+    secret = 'SEC7f8eb6fd58c0ca97134258ce0b4561fda9897ec177fddb8709093064d2fe569b'
+    secret_enc = secret.encode('utf-8')
+    string_to_sign = '{}\n{}'.format(timestamp, secret)
+    string_to_sign_enc = string_to_sign.encode('utf-8')
+    hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
+    sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+    return timestamp, sign
+    # print(timestamp)
+    # print(sign)
+
+if __name__ == "__main__":
+    # 命令行第一个参数为告警内容
+    text_content = "test"
+    data = get_data(text_content)
+    gaojing(data)
